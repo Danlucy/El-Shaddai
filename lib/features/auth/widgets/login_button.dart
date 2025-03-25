5d8ae5f0d1276f9d528e9bb809878c840f2fac33
Daniel <danielongzhien@gmail.com>
2025-03-25 15:22:40 +0800

    d

--- lib/features/auth/widgets/login_button.dart
+++ lib/features/auth/widgets/login_button.dart
@@ -3,32 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SignInButton extends ConsumerWidget {
-  const SignInButton({super.key});
-
+  const SignInButton({required this.mode, super.key});
+  final String mode;
   void signInWithGoogle(BuildContext context, WidgetRef ref) {
     ref.read(authControllerProvider.notifier).signInWithGoogle(context);
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ElevatedButton.icon(
-      onPressed: () => signInWithGoogle(context, ref),
-      icon: Image.asset(
-        'assets/logo/google.png',
-        height: 40,
-        width: 40,
-      ),
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 40),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
+    final bool isGoogle = mode == 'Google';
+    final width = MediaQuery.sizeOf(context).width;
+    return SizedBox(
+      width: width - 20,
+      height: 50,
+      child: ElevatedButton.icon(
+        onPressed: () {
+          isGoogle
+              ? signInWithGoogle(context, ref)
+              : print('Sign in with Apple'); //Add apple heere
+        },
+        icon: isGoogle
+            ? Image.asset(
+                'assets/logo/google.png',
+                height: 40,
+                width: 40,
+              )
+            : Padding(
+                padding: EdgeInsets.all(5),
+                child: Image.asset(
+                  'assets/logo/apple.png',
+                  height: 40,
+                  width: 40,
+                  color: Colors.white,
+                ),
+              ),
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
           ),
         ),
-      ),
-      label: const Text(
-        'Continue with Google',
-        style: TextStyle(fontSize: 18),
+        label: Text(
+          'Continue with $mode',
+          style: const TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
