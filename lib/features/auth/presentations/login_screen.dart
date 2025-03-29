47c5486bfa500d49f5118849ad6a2a9ad2f725a7
Samuel Ong <samuelong135@gmail.com>
2025-03-29 13:10:25 +0800

    feat: added apple login

--- lib/features/auth/presentations/login_screen.dart
+++ lib/features/auth/presentations/login_screen.dart
@@ -1,5 +1,8 @@
+import 'package:el_shaddai/features/auth/controller/auth_controller.dart';
 import 'package:el_shaddai/features/auth/widgets/login_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key, this.from});
@@ -9,30 +12,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  void signInWithGoogle(BuildContext context, WidgetRef ref) {
+    ref.read(authControllerProvider.notifier).signInWithGoogle(context);
+  }
+
+  void signInWithApple(BuildContext context, WidgetRef ref) {
+    ref.read(authControllerProvider.notifier).signInWithApple(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'El Shaddai',
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return Consumer(builder: (context, ref, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'El Shaddai',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/logo/wings_of_freedom.png',
-              height: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const SignInButton()
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/logo/wings_of_freedom.png',
+                height: 200,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SignInButton(
+                text: 'Continue with Google',
+                image: 'assets/logo/google.png',
+                onPressed: () => signInWithGoogle(context, ref),
+              ),
+              const Gap(16),
+              SignInButton(
+                text: 'Continue with Apple',
+                image: 'assets/logo/apple.png',
+                onPressed: () => signInWithApple(context, ref),
+                iconColor: Theme.of(context).colorScheme.onSurface,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- lib/features/auth/widgets/login_button.dart
+++ lib/features/auth/widgets/login_button.dart
@@ -1,22 +1,28 @@
-import 'package:el_shaddai/features/auth/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SignInButton extends ConsumerWidget {
-  const SignInButton({super.key});
+  const SignInButton(
+      {super.key,
+      required this.text,
+      required this.image,
+      required this.onPressed,
+      this.iconColor});
 
-  void signInWithGoogle(BuildContext context, WidgetRef ref) {
-    ref.read(authControllerProvider.notifier).signInWithGoogle(context);
-  }
+  final String text;
+  final String image;
+  final VoidCallback onPressed;
+  final Color? iconColor;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton.icon(
-      onPressed: () => signInWithGoogle(context, ref),
+      onPressed: onPressed,
       icon: Image.asset(
-        'assets/logo/google.png',
+        image,
         height: 40,
         width: 40,
+        color: iconColor,
       ),
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 40),
@@ -26,9 +32,9 @@ class SignInButton extends ConsumerWidget {
           ),
         ),
       ),
-      label: const Text(
-        'Continue with Google',
-        style: TextStyle(fontSize: 18),
+      label: Text(
+        text,
+        style: const TextStyle(fontSize: 18),
       ),
     );
   }
