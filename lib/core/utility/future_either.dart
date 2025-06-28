64f72efabe25bbc94210a2e2e2b7a54e0b0f3cf2
Daniel <danielongzhien@gmail.com>
2025-06-28 15:37:43 +0800

    FINAL RELEASE

--- lib/core/utility/future_either.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:fpdart/fpdart.dart';
-
-import 'failure.dart';
-
-typedef FutureEither<T> = Future<Either<Failure, T>>;
-typedef FutureVoid = FutureEither<void>;
-typedef MyBuilder = void Function(
-    BuildContext context, void Function() methodFromChild);
